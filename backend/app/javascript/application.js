bec0de9b7b253d0a455c401cfbcf838009c17971
Iván Madera <maderamartinezivan@gmail.com>
2025-04-26 11:38:54 -0500

    navbar diseñado

--- backend/app/javascript/application.js
+++ backend/app/javascript/application.js
@@ -1,3 +1,20 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Import básico de Rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Control de modo oscuro
+document.addEventListener("turbo:load", () => {
+  const toggleButton = document.getElementById("theme-toggle");
+
+  const applyTheme = (theme) => document.body.setAttribute('data-theme', theme);
+
+  const savedTheme = localStorage.getItem("theme") || "light";
+  applyTheme(savedTheme);
+
+  toggleButton.addEventListener("click", () => {
+    const currentTheme = localStorage.getItem("theme") || "light";
+    const newTheme = currentTheme === "light" ? "dark" : "light";
+    localStorage.setItem("theme", newTheme);
+    applyTheme(newTheme);
+  });
+});
