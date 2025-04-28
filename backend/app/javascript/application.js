49bad4c1eeb4f70991db2d0b23957318d4f6f947
Iván Madera <maderamartinezivan@gmail.com>
2025-04-28 09:38:31 -0500

    bug theme

--- backend/app/javascript/application.js
+++ backend/app/javascript/application.js
@@ -5,16 +5,22 @@ import "controllers"
 // Control de modo oscuro
 document.addEventListener("turbo:load", () => {
   const toggleButton = document.getElementById("theme-toggle");
+  const themeToggleXl = document.getElementById("theme-toggle-xl");
 
   const applyTheme = (theme) => document.body.setAttribute('data-theme', theme);
 
   const savedTheme = localStorage.getItem("theme") || "light";
   applyTheme(savedTheme);
 
-  toggleButton.addEventListener("click", () => {
+  const toggleTheme = () => {
     const currentTheme = localStorage.getItem("theme") || "light";
     const newTheme = currentTheme === "light" ? "dark" : "light";
-    localStorage.setItem("theme", newTheme);
-    applyTheme(newTheme);
-  });
+    localStorage.setItem("theme", newTheme); // Guardar el nuevo tema
+    applyTheme(newTheme); // Aplicar el nuevo tema
+  } 
+
+  // Añadir el event listener para ambos botones
+  toggleButton.addEventListener("click", toggleTheme);
+  themeToggleXl.addEventListener("click", toggleTheme);
 });
+
