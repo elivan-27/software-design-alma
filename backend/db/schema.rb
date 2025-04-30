9f9e1d692d78cc47fcdb01dead3af9569b598c7b
Iván Madera <maderamartinezivan@gmail.com>
2025-04-30 10:02:07 -0500

    device

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_25_203424) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_30_145820) do
   create_table "t_shirts", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -23,10 +23,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_25_203424) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.integer "age"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "phone"
+    t.text "address"
+    t.string "role"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
