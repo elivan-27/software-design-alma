39e529f7e5e4550f5c42f4bc59aec73af82c04af
GooIvan <maderamartinezivan@gmail.com>
2025-05-07 16:57:58 -0500

    .

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -56,17 +56,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_05_151523) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
-  create_table "t_shirts", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "size"
-    t.string "color"
-    t.decimal "price"
-    t.integer "stock"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
