17d66b8320532b2ae3ead74e1647f7492ba3fa61
BrianOrtegaa <brianorte8@gmail.com>
2025-05-19 13:38:49 -0500

    add controllers

---
+++ backend/app/controllers/carts_controller.rb
@@ -0,0 +1,21 @@
+class CartsController < ApplicationController
+  def show
+    @cart = current_cart
+  end
+
+  private
+
+  def current_cart
+    if session[:cart_id]
+      Cart.find_by(id: session[:cart_id]) || create_cart
+    else
+      create_cart
+    end
+  end
+
+  def create_cart
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
+end
