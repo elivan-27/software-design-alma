eb835d5bfdd1b5bbfe110cc68ea4161552be004d
GooIvan <maderamartinezivan@gmail.com>
2025-05-19 16:32:36 -0500

    eliminar tabla tshirt

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -53,20 +53,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_17_152325) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id", null: false
-    t.string "size"
     t.text "sizes"
     t.index ["category_id"], name: "index_products_on_category_id"
-  end
-
-  create_table "t_shirts", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "size"
-    t.string "color"
-    t.decimal "price"
-    t.integer "stock"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
