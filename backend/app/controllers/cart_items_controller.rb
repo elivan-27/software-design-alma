78f922b351ab79bd57bc0b0ea1aad3ce343cf585
GooIvan <maderamartinezivan@gmail.com>
2025-05-21 20:53:44 -0500

    bug carrito arreglado

--- backend/app/controllers/cart_items_controller.rb
+++ backend/app/controllers/cart_items_controller.rb
@@ -2,31 +2,34 @@ class CartItemsController < ApplicationController
   def create
     cart = current_cart
     product = Product.find(params[:product_id])
-    selected_sizes = Array(params[:sizes]) # Asegura que sea array aunque venga nil
-
-      if selected_sizes.empty?
-        redirect_back fallback_location: root_path, alert: t("size_obligatory")
-        return
-      end
-
+    selected_sizes = Array(params[:sizes])
+  
+    if selected_sizes.empty?
+      redirect_back fallback_location: root_path, alert: t("size_obligatory")
+      return
+    end
+  
     selected_sizes.each do |size|
       item = cart.cart_items.find_by(product_id: product.id, size: size)
-
+  
       if item
         item.increment(:quantity)
       else
         item = cart.cart_items.build(product: product, size: size, quantity: 1)
       end
-
+  
       item.save
     end
-
+  
+    flash[:item_added] = true # 🔥 Aquí
     redirect_back fallback_location: root_path
   end
+  
 
   def destroy
     item = current_cart.cart_items.find(params[:id])
     item.destroy
+    flash[:item_added] = true # 🔥 Aquí
     redirect_back fallback_location: root_path
   end
 
